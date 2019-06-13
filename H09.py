4da00e618edaf3b4a29a58a82f744eeb8c3d7e27
arshad408 <arshadachu408@gmail.com>
2019-06-13 11:00:17 +0530

    Update H09.py

--- H09.py
+++ H09.py
@@ -1,10 +1,10 @@
 a=int(input())
 b=list(map(int,input().split()))
 l=max(b)
-d,f=0,0
+e,f=0,0
 for i in range(0,len(b)):
   for j in range(i+1,len(b)):
-    if abs(b[i]+y[j])<l:
-      d,f=b[i],b[j]
-      l=abs(d+f)
-print(d,f)
+    if abs(b[i]+b[j])<l:
+      e,f=b[i],b[j]
+      l=abs(e+f)
+print(e,f)
