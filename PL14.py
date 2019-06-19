1d6ccff274b1d87b49cb30f9dcb4cec426de069c
arshad408 <arshadachu408@gmail.com>
2019-06-19 15:31:53 +0530

    Update PL14.py

--- PL14.py
+++ PL14.py
@@ -1,8 +1,11 @@
-s=int(input())
-a=input()
-t=0
-b=['a','e','i','o','u']
-for i in s1:
-  if(i in b):
-    a= a.replace(i,"")
-print(a[::-1])
+    
+a=int(input())
+x=input()  
+h=[]
+x=list(x)  
+for j in x:
+      if(j=='A' or j=='E' or j=='I' or j=='O' or j=='U' or j=='a' or j=='e' or j=='i' or j=='o' or j=='u'):
+          x.remove(j)
+          h.append(j) 
+I=x[ ::-1 ]
+print(*I,sep=""); 
