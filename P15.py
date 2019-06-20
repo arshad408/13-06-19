8c8e6e01e64fc5220fcefeb54dc08a70515159bc
arshad408 <arshadachu408@gmail.com>
2019-06-20 14:42:58 +0530

    Create P15.py

---
+++ P15.py
@@ -0,0 +1,16 @@
+x=int(input())
+y=list(map(int,input().split()))
+n=s=[]
+for i in range(0,x):
+    n=list(bin(y[i]))
+    n=n[2:]
+    s.append(n)
+s=sorted(s)
+s=s[::-1]
+for i in range(0,x):
+    p=1
+    z=0
+    for j in range(len(s[i])-1,-1,-1):
+        z=z+(int(s[i][j])*p)
+        y=y*2
+    print(z)
