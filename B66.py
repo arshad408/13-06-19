21e7730338d07345cc7a073b6ea6b68bb2aed03b
arshad408 <arshadachu408@gmail.com>
2019-07-01 16:12:05 +0530

    Create B66.py

---
+++ B66.py
@@ -0,0 +1,15 @@
+def isPrime(n): 
+	if (n <= 1): 
+		return False
+	for i in range(2, n): 
+		if (n % i == 0): 
+			return False
+
+	return True
+if isPrime(int(input())): 
+	print ("true") 
+else: 
+	print ("false") 
+	 
+
+
