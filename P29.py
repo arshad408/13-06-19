07e6fd6cc7d1cfb50ad053b1e5a1281e66ed5d19
arshad408 <arshadachu408@gmail.com>
2019-07-18 12:25:19 +0530

    Update P29.py

--- P29.py
+++ P29.py
@@ -1,11 +1,17 @@
-
-a=int(input())
-li=list(map(int,input().split()))
-li.sort()
-x=0
-y=0
-for i in range(len(li)):
-    if li[i]>=x:
-        y=y+1
-    x=x+li[i]
-print(y)
+def numb(x):
+    m=0
+    y=0
+    l=[]
+    while m<100 and m<x:
+        c=0
+        for x in str(x-m):
+            c+=int(x)
+        if c+(x-m)==x:
+            y+=1
+            l.append(x-m)
+        m+=1
+    print(y)  
+    for m in l:
+            print(m)
+x=int(input())
+numb(x)
