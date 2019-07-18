bb4eb8bd217bd189d8facb83601e85f42c7a7d7d
arshad408 <arshadachu408@gmail.com>
2019-07-18 12:28:24 +0530

    Update P29.py

--- P29.py
+++ P29.py
@@ -1,17 +1,12 @@
-def numb(x):
-    m=0
-    y=0
-    l=[]
-    while m<100 and m<x:
-        c=0
-        for x in str(x-m):
-            c+=int(x)
-        if c+(x-m)==x:
-            y+=1
-            l.append(x-m)
-        m+=1
-    print(y)  
-    for m in l:
-            print(m)
-x=int(input())
-numb(x)
+
+ar = int(input())
+tr = int(ar/2)
+ra = []
+for i in range(ar, tr, -1):
+    j = str(i)
+    if i + sum([int(ka) for ka in j]) == ar:
+        print(1)
+        print(i)
+        break
+else:
+    print(0) 
